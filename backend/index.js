97c1f55264d16456121bf6074bba27529f087a68
emheron <m.emmanuel.heron@gmail.com>
2023-08-26 00:11:31 -0500

    initial setup for medical rbac system

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/medicalRBAC', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
